6d2a46ff94fc42f810c5c7867c9d4069fe939767
Jf-felipe14 <123562474+Jf-felipe14@users.noreply.github.com>
2023-11-30 19:28:19 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,30 +1,40 @@
 import os
 import shutil
+import sys
 
-origem=os.getcwd()
-lista=os.listdir(os.getcwd())
-remove=['index.py','.gitignore','.gitattributes']
-lista=[i for i in lista if i not in remove]
+origen=os.getcwd()
+list=os.listdir(origen)
+remove=['.gitignore','index copy.py','index.py']
+lis=[i for i in list if i not in remove]
+arquivo=[]
+for i in lis:
+    if os.path.isfile(i):
+        arquivo.append(i)
+    else:
+        continue
 
-for i in lista:
-    print(i)
+class Arquivo:
+    def __init__(self,nome,numero):
+        self.nome=nome
+        self.numero=numero
 
-try:
-    for i in lista:
-        j=i.split(".")
-        origem1=origem+f"\\{i}"
-        
 
-                
-        if not os.path.exists(j[1]):
-            os.makedirs(j[1])
-            dd=os.getcwd() 
-            destino=dd+f"\\{j[1]}"
-        else:
-            dd=os.getcwd()
-            destino=dd+f"\\{j[1]}"
-        shutil.move(origem1,destino)
-except:
-    print('A sua Pasta Não contem Nenhum arquivo...')
-
-# Apenas uma atualização
+for j,i in enumerate(arquivo):
+    nome,exten1=os.path.splitext(i)
+    
+    exten=exten1[1:]
+    if os.path.exists(exten):
+        try:
+            shutil.move(f'{origen}/{i}',f'{origen}/{exten}')
+        except:
+            j+=5
+            j=str(j)
+            os.rename(i,nome+j+exten1)
+            try:
+                shutil.move(f'{origen}/{i}',f'{origen}/{exten}')
+            except:
+                shutil.move(f'{origen}/{i}',f'{origen}/{exten}')
+                        
+    else:
+        os.makedirs(exten)
+        shutil.move(f'{origen}/{i}',f'{origen}/{exten}')
